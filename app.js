00dbfe580437d0218b98cc8b38f44b840505325e
Fabio <sstrmn1@gmail.com>
2024-04-22 15:09:55 -0400

    [backend] crear estructura general de servidor [db] base de datos aun no creada [frontend] crear carpeta de vistas con vistas basicas de tablas de usuarios

---
+++ app.js
@@ -0,0 +1,18 @@
+//Aplicacion Modelo-Vista-Controlador con MariaDB, MySQL, Express con operaciones CRUD
+const express = require("express"); //Importar express que es un framework de Node.js que nos permite crear servidores web
+const exphbs = require("express-handlebars"); //Importar express-handlebars que es un motor de plantillas para Express que nos permite crear vistas de manera sencilla
+const app = express(); //Crear una instancia de express para poder utilizar sus metodos
+const port = process.env.PORT || 3000; //Definir el puerto en el que se va a ejecutar el servidor
+app.use(express.json()); //Permitir a express leer y enviar datos en formato JSON
+app.use(express.urlencoded({ extended: true })); //Permitir a express leer y enviar datos en formato URL-encoded
+app.use(express.static("public")); 
+app.engine("hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en linea en puerto: ${port}`);
+});
---
+++ db.js
@@ -0,0 +1,30 @@
+const Sequelize = require("sequelize");
+const db = new Sequelize("ums", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+}); //Crear una instancia de Sequelize para poder conectarnos a la base de datos
+
+module.exports = db; //Exportar la instancia de Sequelize para poder utilizarla en otros archivos
+
+// ¿Que es sequelize?
+// Sequelize es un ORM (Object-Relational Mapping) que nos permite interactuar
+// con bases de datos relacionales utilizando objetos en lugar de sentencias SQL.
+// Un ejemplo de un objeto en Sequelize insetar un registro en una tabla de la base de datos
+// seria:
+// const db = require("./db");
+// const { DataTypes } = require("sequelize");
+// const User = db.define("User", {
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   email: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+// });
+// User.create({
+//   name: "John Doe",
+//   email: "
+// });
+// En este ejemplo, creamos un modelo de usuario con dos campos: name y email.
